7b82d3d13005b4e501dc84ae94d215addaa36664
AurelieVidal <123560566+AurelieVidal@users.noreply.github.com>
2023-11-29 10:19:55 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo dpkg --configure -a
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo docker run hello-world
+git clone https://github.com/AurelieVidal/DevOps2.git
+cd DevOps2
+docker compose up
